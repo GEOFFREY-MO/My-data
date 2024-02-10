c1b14b7445530e0a0ac47aefda6238a75e0c3080
GEOFFREY-MO <117658829+GEOFFREY-MO@users.noreply.github.com>
2024-02-10 16:42:41 +0300

    Update and rename bgremover.ipynb to bgremover.py

---
+++ bgremover.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from rembg import remove
+from PIL import Image
+
+def remove_background(input_image):
+    # Remove the background from the input image
+    output_image = remove(input_image)
+    return output_image
+
+def main():
+    st.title('DAVETECH-BG-REMOVER')
+    st.markdown('Upload an image and remove its background!')
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Read the uploaded image file
+        input_image = Image.open(uploaded_file)
+
+        # Display the uploaded image
+        st.image(input_image, caption="Uploaded Image", use_column_width=True)
+
+        # Check if the user clicked the 'Remove Background' button
+        if st.button('Remove Background'):
+            # Call the function to remove the background
+            output_image = remove_background(input_image)
+
+            # Display the output image with the background removed
+            st.image(output_image, caption="Output Image", use_column_width=True)
+            st.success('Background removed successfully!')
+
+if __name__ == "__main__":
+    main()
